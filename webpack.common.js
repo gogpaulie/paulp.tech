297e1812a531eb53dcc495fafc8bf139292b05f3
GoG_Paulie <ppolih21@gmail.com>
2020-10-14 21:06:50 -0400

    weboack test

--- webpack.common.js
+++ webpack.common.js
@@ -1,35 +1,33 @@
-const Webpack = require("webpack");
+const Webpack = require('webpack');
 module.exports = {
   entry: {
-    vendor: "./src/vendor.js",
-    main: "./src/index.js"
+    vendor: './src/vendor.js',
+    main: './src/index.js',
   },
   module: {
     rules: [
       {
         test: /\.html$/,
-        use: ["html-loader"]
+        use: ['html-loader'],
       },
       {
-        test: /\.(jpg|jpeg|png|gif|svg|pdf)$/,
+        test: /\.(png|svg|jpg|gif|pdf)$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[name].[hash].[ext]",
-              outputPath: "assets",
-              esModule: false
-            }
-          }
-        ]
-      }
-    ]
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
   },
   // This config allows to use jQuery $ sign
   plugins: [
     new Webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
-    })
-  ]
+      $: 'jquery',
+      jQuery: 'jquery',
+    }),
+  ],
 };
