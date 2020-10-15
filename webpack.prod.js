b2d0a530da9ec44ff8c7f256bd86378b81b66dd4
GoG_Paulie <ppolih21@gmail.com>
2020-10-14 21:11:31 -0400

    124141

--- webpack.prod.js
+++ webpack.prod.js
@@ -44,6 +44,17 @@ module.exports = merge(common, {
           'sass-loader',
         ], //1. Turns sass into css
       },
+      {
+        test: /\.(png|svg|jpg|gif|pdf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
     ],
   },
 });
